7007c2f40693f42c61a6ff5410b83cfbbd40b200
0-TDH-0 <t.dev.sec@gmail.com>
2024-01-06 20:11:25 -0600

    Created all screens and updated main page with relevant buttons

---
+++ appScreens/lostList.swift
@@ -0,0 +1,8 @@
+//
+//  lostList.swift
+//  Back Home
+//
+//  Created by Tommy Hale on 1/6/24.
+//
+
+import Foundation
---
+++ appScreens/lostScreen.swift
@@ -0,0 +1,8 @@
+//
+//  lostScreen.swift
+//  Back Home
+//
+//  Created by Tommy Hale on 1/6/24.
+//
+
+import Foundation
